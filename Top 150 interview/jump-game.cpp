94444b35727e9935623c09d1d07a94543f6aa1c4
RaresRacsan <82086767+RaresRacsan@users.noreply.github.com>
2024-09-18 16:56:13 +0300

    Create jump-game.cpp

---
+++ Top 150 interview/jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+         int n = nums.size();
+        if (n == 0 ) return false;  
+        if ( n == 1 && nums[0] == 0 ) return true;  
+        int jumps = 0;
+        int current_end = 0;
+        int farthest = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
+                if (current_end >= n - 1) break;  
+            }
+        }  
+        return (current_end >= n - 1) ? true  : false;
+    }
+};
